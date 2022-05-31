89abec6caabea0464b6d3f4f04270f837858708a
Michael Kryukov <kryukov.ms@yandex.ru>
2022-05-31 23:29:14 +0300

    feat: refactored code to be better compatible with mocking; added tests (#15)
    * feat: refactored code to be better compatible with mocking; added tests
    
    * fix: safe testing for any python version

---
+++ tests/test_mocking.py
@@ -0,0 +1,40 @@
+from unittest.mock import patch
+from bson import ObjectId
+from pymongo.results import UpdateResult
+import pytest
+from mongomock_motor import AsyncMongoMockClient
+
+
+async def sample_function(collection):
+    result = await collection.update_one(
+        filter={'_id': ObjectId()},
+        update={'$set': {'field': 'value'}},
+        upsert=True,
+    )
+
+    if result.acknowledged is False:
+        raise RuntimeError()
+
+
+def async_wrapper(value):
+    async def wrapper(*args, **kwargs):
+        return value
+    return wrapper
+
+
+@pytest.mark.asyncio
+async def test_patch():
+    collection = AsyncMongoMockClient()['test']['test']
+
+    with patch('mongomock_motor.AsyncMongoMockCollection.update_one', new=async_wrapper(UpdateResult({}, False))):
+        with pytest.raises(RuntimeError):
+            await sample_function(collection)
+
+
+@pytest.mark.asyncio
+async def test_patch_object():
+    collection = AsyncMongoMockClient()['test']['test']
+
+    with patch.object(collection, 'update_one', new=async_wrapper(UpdateResult({}, False))):
+        with pytest.raises(RuntimeError):
+            await sample_function(collection)
