98a66c673c32e1d0a32256b124e79cd372f094e0
Michael Kryukov <mkrukov@bestplace.pro>
2024-02-25 19:13:31 +0300

    feat: add option to install module from git

--- setup.py
+++ setup.py
@@ -6,38 +6,40 @@
 """
 
 import os
+
 import setuptools
 
+if os.environ.get('GITHUB_REF_TYPE') == 'tag':
+    assert os.environ.get('GITHUB_REF_NAME')
+    VERSION = os.environ['GITHUB_REF_NAME'].lstrip('v')
+else:
+    VERSION = '0+unknown'
 
-assert os.environ.get('GITHUB_REF_TYPE') == 'tag'
-assert os.environ.get('GITHUB_REF_NAME')
-VERSION = os.environ['GITHUB_REF_NAME'].lstrip('v')
 
-
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 
 setuptools.setup(
-    name="mongomock_motor",
+    name='mongomock_motor',
     version=VERSION,
-    author="Michael Krukov",
-    author_email="krukov.michael@ya.ru",
-    keywords=["library", "mongodb"],
-    description="Library for mocking AsyncIOMotorClient built on top of mongomock.",
+    author='Michael Krukov',
+    author_email='krukov.michael@ya.ru',
+    keywords=['library', 'mongodb'],
+    description='Library for mocking AsyncIOMotorClient built on top of mongomock.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/michaelkryukov/mongomock_motor",
+    long_description_content_type='text/markdown',
+    url='https://github.com/michaelkryukov/mongomock_motor',
     packages=setuptools.find_packages(include=('mongomock_motor',)),
     install_requires=[
-        "mongomock>=3.23.0,<5.0.0",
+        'mongomock>=3.23.0,<5.0.0',
     ],
-    python_requires=">=3.6",
+    python_requires='>=3.6',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
     ],
 )
